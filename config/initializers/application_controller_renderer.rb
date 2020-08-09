711fcc171e42f4ad5876e20bc330ea661cb23548
YutoSuzuki1024 <yuto.suzuki1024@gmail.com>
2020-08-09 18:58:08 +0900

    Finish Skill-Up

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = 'e502691b09b875e771ab0325aec70637f2f30d236fba74ea5c421a4208d2fecb8fe72f67ffd9aa16caec74df995f4c41fbcecd0d195ef4d269c2ad630828026d'
